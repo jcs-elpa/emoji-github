c1eb1625452f896f3fad1048a8563411b50da542
jenchieh <jcs090218@gmail.com>
2020-03-22 16:34:45 +0800

    Bump version.

--- emoji-github.el
+++ emoji-github.el
@@ -6,7 +6,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Description: Display list of GitHub's emoji.  (cheat sheet)
 ;; Keyword: list github emoji display handy
-;; Version: 0.2.0
+;; Version: 0.2.1
 ;; Package-Requires: ((emacs "24.4") (emojify "1.0") (request "0.3.0"))
 ;; URL: https://github.com/jcs090218/emoji-github
 
